febb9ef93e923ed02891f4d0c3dcf28d156e9789
x1m1x <59328260+x1m1x@users.noreply.github.com>
2020-07-09 15:01:01 +0600

    Update settings.py

--- mytestask/settings.py
+++ mytestask/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'eaak(+=!xwhdg%yb8@-fit#iun)b0_!8_uy30ug(ol@-2-5_xi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
